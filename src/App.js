937a6a83ab3e7c70a8e87baa64054e483aa62a30
masachetti <50852444+masachetti@users.noreply.github.com>
2022-09-28 17:29:24 -0400

    Developed 'Binary to Decimal' component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import BinToDec from './pages/BinToDec';
 
 function App() {
   return (
-    <>Hello World</>
+    <BinToDec/>
   );
 }
 
---
+++ src/pages/BinToDec.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+function parseToDecimal(binaryValue) {
+  let reversedBits = binaryValue.split("").reverse();
+  let decimal = 0;
+  for (const [bitIndex, bit] of reversedBits.entries()) {
+    let intBit = parseInt(bit);
+    decimal += bitIndex ? bitIndex ** 2 * intBit : intBit;
+  }
+  return decimal;
+}
+
+function parseToBinary(value) {
+  let decimalValue = parseInt(value);
+  let bitArray = [];
+  while (decimalValue > 1){
+    bitArray.push(decimalValue%2);
+    decimalValue = Math.floor(decimalValue/2);
+  }
+  bitArray.push(decimalValue);
+  return bitArray.reverse().join('')
+}
+
+export default function BinToDec() {
+  const [value, setValue] = useState({
+    currentValue: 0,
+    valueFormat: "dec",
+  });
+
+  const handleValueChange = function (e, format) {
+    setValue({
+      currentValue: e.target.value,
+      valueFormat: format,
+    });
+  };
+  const { currentValue, valueFormat } = value;
+  const binValue =
+    valueFormat === "bin" ? currentValue : parseToBinary(currentValue);
+  const decValue =
+    valueFormat === "dec" ? currentValue : parseToDecimal(currentValue);
+
+  return (
+    <div>
+      <label htmlFor="binary">Binary: </label>
+      <input
+        id="binary"
+        type="text"
+        value={binValue}
+        onChange={(e) => handleValueChange(e, "bin")}
+      />
+      <br />
+      <label htmlFor="dec">Decimal: </label>
+      <input
+        id="dec"
+        type="text"
+        value={decValue}
+        onChange={(e) => handleValueChange(e, "dec")}
+      />
+    </div>
+  );
+}
